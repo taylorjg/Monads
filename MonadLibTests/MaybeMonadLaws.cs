70999faa247b4188d0316427382617f4b8f4af69
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-09-22 00:22:55 +0100

    Added unit tests for Maybe re the monad laws

---
+++ MonadLibTests/MaybeMonadLaws.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MonadLib;
+using NUnit.Framework;
+
+namespace MonadLibTests
+{
+    [TestFixture]
+    internal class MaybeMonadLaws
+    {
+        [Test]
+        public void BindLeftIdentity()
+        {
+            // (return x) >>= f == f x
+            const int x = 5;
+            Func<int, Maybe<int>> f = n => Maybe.Just(n * n);
+            var actual1 = Maybe.Unit(x).Bind(f);
+            var actual2 = f(x);
+            Assert.That(actual1.IsJust, Is.True);
+            Assert.That(actual2.IsJust, Is.True);
+            Assert.That(actual1.FromJust, Is.EqualTo(actual2.FromJust));
+        }
+
+        [Test]
+        public void BindRightIdentity()
+        {
+            // m >>= return == m
+            var m = Maybe.Unit(5);
+            var actual1 = m.Bind(Maybe.Unit);
+            var actual2 = m;
+            Assert.That(actual1.IsJust, Is.True);
+            Assert.That(actual2.IsJust, Is.True);
+            Assert.That(actual1.FromJust, Is.EqualTo(actual2.FromJust));
+        }
+
+        [Test]
+        public void BindAssociativity()
+        {
+            // (m >>= f) >>= g == m >>= (\x -> f x >>= g)
+            Func<int, Maybe<int>> f = n => Maybe.Just(n * n);
+            Func<int, Maybe<string>> g = n => Maybe.Just(Convert.ToString(n));
+            var m = Maybe.Unit(5);
+            var actual1 = m.Bind(f).Bind(g);
+            var actual2 = m.Bind(x => f(x).Bind(g));
+            Assert.That(actual1.IsJust, Is.True);
+            Assert.That(actual2.IsJust, Is.True);
+            Assert.That(actual1.FromJust, Is.EqualTo(actual2.FromJust));
+        }
+    }
+}
