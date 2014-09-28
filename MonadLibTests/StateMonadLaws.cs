30c4acfc90e5901b6bc011311e6cd86eb39b1fb3
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-09-28 10:37:10 +0100

    Added unit tests for the Reader monad laws

--- MonadLibTests/StateMonadLaws.cs
+++ MonadLibTests/StateMonadLaws.cs
@@ -13,11 +13,11 @@ namespace MonadLibTests
             // (return x) >>= f == f x
             const double x = 5;
             Func<double, State<int, string>> f = d => State<int>.Return(Convert.ToString(d * d));
-            var actual1 = State<int>.Return(x).Bind(f);
-            var actual2 = f(x);
+            var stateMonad1 = State<int>.Return(x).Bind(f);
+            var stateMonad2 = f(x);
             const int s = 10;
-            var pair1 = actual1.RunState(s);
-            var pair2 = actual2.RunState(s);
+            var pair1 = stateMonad1.RunState(s);
+            var pair2 = stateMonad2.RunState(s);
             Assert.That(pair1, Is.EqualTo(pair2));
         }
 
@@ -26,11 +26,11 @@ namespace MonadLibTests
         {
             // m >>= return == m
             var m = State<int>.Return('X');
-            var actual1 = m.Bind(State<int>.Return);
-            var actual2 = m;
+            var stateMonad1 = m.Bind(State<int>.Return);
+            var stateMonad2 = m;
             const int s = 10;
-            var pair1 = actual1.RunState(s);
-            var pair2 = actual2.RunState(s);
+            var pair1 = stateMonad1.RunState(s);
+            var pair2 = stateMonad2.RunState(s);
             Assert.That(pair1, Is.EqualTo(pair2));
         }
 
@@ -41,11 +41,11 @@ namespace MonadLibTests
             Func<int, State<int, double>> f = n => State<int>.Return(Convert.ToDouble(n * n + 0.1m));
             Func<double, State<int, string>> g = d => State<int>.Return(Convert.ToString(d));
             var m = State<int>.Return(5);
-            var actual1 = m.Bind(f).Bind(g);
-            var actual2 = m.Bind(x => f(x).Bind(g));
+            var stateMonad1 = m.Bind(f).Bind(g);
+            var stateMonad2 = m.Bind(x => f(x).Bind(g));
             const int s = 10;
-            var pair1 = actual1.RunState(s);
-            var pair2 = actual2.RunState(s);
+            var pair1 = stateMonad1.RunState(s);
+            var pair2 = stateMonad2.RunState(s);
             Assert.That(pair1, Is.EqualTo(pair2));
         }
     }
