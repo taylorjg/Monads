d930ee334b1117daee5c8a03206019032f8e6c55
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-11-10 19:22:57 +0000

    Modified Either demo program to use a type alias for Either<string>

--- DemoPrograms/EitherMovieReview/Program.cs
+++ DemoPrograms/EitherMovieReview/Program.cs
@@ -5,6 +5,7 @@ using MonadLib;
 namespace EitherMovieReview
 {
     using AssociationList = Dictionary<string, Maybe<string>>;
+    using EitherError = Either<string>;
 
     internal class Program
     {
@@ -41,24 +42,15 @@ namespace EitherMovieReview
 
         private static Either<string, string> Lookup(AssociationList alist, string key)
         {
+            var keyWithEmptyValue = EitherError.Left<string>(string.Format("Found key \"{0}\" but its value is empty", key));
+            var keyWithNoValue = EitherError.Left<string>(string.Format("Found key \"{0}\" but it has no value", key));
+            var keyNotFound = EitherError.Left<string>(string.Format("Failed to find a value for key \"{0}\"", key));
+
             return alist.GetValue(key).Match(
                 v => v.Match(
-                    s =>
-                        {
-                            if (!string.IsNullOrEmpty(s)) return Either<string>.Right(s);
-                            var msg = string.Format("Found key \"{0}\" but its value is empty", key);
-                            return Either<string>.Left<string>(msg);
-                        },
-                    () =>
-                        {
-                            var msg = string.Format("Found key \"{0}\" but it has no value", key);
-                            return Either<string>.Left<string>(msg);
-                        }),
-                () =>
-                    {
-                        var msg = string.Format("Failed to find a value for key \"{0}\"", key);
-                        return Either<string>.Left<string>(msg);
-                    });
+                    s => string.IsNullOrEmpty(s) ? keyWithEmptyValue : EitherError.Right(s),
+                    () => keyWithNoValue),
+                () => keyNotFound);
         }
     }
 }
