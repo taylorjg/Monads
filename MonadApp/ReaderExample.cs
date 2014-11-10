6ae0e479757ecc1a3c4c11f61d538328e821c525
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-11-10 01:11:12 +0000

    Added localExample from Real World Haskell page 432

--- MonadApp/ReaderExample.cs
+++ MonadApp/ReaderExample.cs
@@ -15,6 +15,27 @@ namespace Monads
 
             var a = reader.RunReader(2);
             Console.WriteLine("reader.RunReader(2): {0}", a);
+
+            Console.WriteLine(LocalExample().RunReader("Fred"));
+
+            // TODO: add a better example
+            // e.g. see https://hackage.haskell.org/package/mtl-1.1.0.2/docs/Control-Monad-Reader.html
+            // Example 1: Simple Reader Usage
+            // Example 2: Modifying Reader Content With local
+        }
+
+        private static Reader<string, string> MyName(string step)
+        {
+            return Reader<string>.Ask().Bind(
+                name => Reader<string>.Return(string.Format("{0}, I am {1}", step, name)));
+        }
+
+        private static Reader<string, Tuple<string, string, string>> LocalExample()
+        {
+            return MyName("First").Bind(
+                a => Reader.Local(r => r + "dy", MyName("Second")).Bind(
+                    b => MyName("Third").Bind(
+                        c => Reader<string>.Return(Tuple.Create(a, b, c)))));
         }
     }
 }
