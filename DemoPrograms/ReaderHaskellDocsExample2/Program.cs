8d4465a1c417e35843145f8dc9fd70e9bfea939b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-11-11 14:42:45 +0000

    Another tweak to one of the demo programs

--- DemoPrograms/ReaderHaskellDocsExample2/Program.cs
+++ DemoPrograms/ReaderHaskellDocsExample2/Program.cs
@@ -8,21 +8,28 @@ namespace ReaderHaskellDocsExample2
         private static void Main()
         {
             const string s = "12345";
-            var modifiedLen = CalculateModifiedContentLen().RunReader(s);
-            var len = CalculateContentLen().RunReader(s);
+            var modifiedLen = CalculateModifiedContentLen.RunReader(s);
+            var len = CalculateContentLen.RunReader(s);
             Console.WriteLine("Modified 's' length: {0}", modifiedLen);
             Console.WriteLine("Original 's' length: {0}", len);
         }
 
-        private static Reader<string, int> CalculateContentLen()
+        private static Reader<string, int> CalculateContentLen
         {
-            return Reader<string>.Ask().Bind(
-                content => Reader<string>.Return(content.Length));
+            get
+            {
+                return Reader<string>.Ask().Bind(
+                    content => Reader<string>.Return(content.Length));
+            }
         }
 
-        private static Reader<string, int> CalculateModifiedContentLen()
+        private static Reader<string, int> CalculateModifiedContentLen
         {
-            return Reader.Local(content => "Prefix " + content, CalculateContentLen());
+            get
+            {
+                Func<string, Func<string, string>> partiallyAppliedStringConcatenation = s1 => s2 => s1 + s2;
+                return Reader.Local(partiallyAppliedStringConcatenation("Prefix "), CalculateContentLen);
+            }
         }
     }
 }
