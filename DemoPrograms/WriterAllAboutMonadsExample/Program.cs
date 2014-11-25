083446989020c1a09b529756f6783290285802d6
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-11-25 12:23:27 +0000

    Implemented FilterAll

--- DemoPrograms/WriterAllAboutMonadsExample/Program.cs
+++ DemoPrograms/WriterAllAboutMonadsExample/Program.cs
@@ -8,7 +8,7 @@ namespace WriterAllAboutMonadsExample
 {
     using WriterEntry = Writer<ListMonoid<Entry>, ListMonoidAdapter<Entry>, Entry>;
     using U1 = Writer<ListMonoid<Entry>, ListMonoidAdapter<Entry>, Entry, Unit>;
-    using U2 = Writer<ListMonoid<Entry>, ListMonoidAdapter<Entry>, Entry, IEnumerable<Entry>>;
+    using U2 = Writer<ListMonoid<Entry>, ListMonoidAdapter<Entry>, Entry, ListMonoid<Entry>>;
     using U3 = Writer<ListMonoid<Entry>, ListMonoidAdapter<Entry>, Entry, Maybe<Packet>>;
     using U4 = Writer<ListMonoid<Entry>, ListMonoidAdapter<Entry>, Entry, IEnumerable<Packet>>;
 
@@ -31,7 +31,7 @@ namespace WriterAllAboutMonadsExample
             return WriterEntry.Tell(new ListMonoid<Entry>(new[] {new Entry(1, s)}));
         }
 
-        private static U2 MergeEntries(IEnumerable<Entry> entries1, IEnumerable<Entry> entries2)
+        private static U2 MergeEntries(ListMonoid<Entry> entries1, ListMonoid<Entry> entries2)
         {
             return null;
         }
@@ -54,7 +54,7 @@ namespace WriterAllAboutMonadsExample
 
         private static Writer<ListMonoid<TA>, ListMonoidAdapter<TA>, TA, IEnumerable<TC>> GroupSame<TA, TB, TC>(
             ListMonoid<TA> a,
-            Func<TA, TA, Writer<ListMonoid<TA>, ListMonoidAdapter<TA>, TA, TA>> merge,
+            Func<ListMonoid<TA>, ListMonoid<TA>, Writer<ListMonoid<TA>, ListMonoidAdapter<TA>, TA, ListMonoid<TA>>> merge,
             IEnumerable<TB> bs,
             Func<TB, Writer<ListMonoid<TA>, ListMonoidAdapter<TA>, TA, TC>> fn)
         {
@@ -63,10 +63,28 @@ namespace WriterAllAboutMonadsExample
 
         private static U4 FilterAll(IEnumerable<Rule> rules, IEnumerable<Packet> packets)
         {
+            Func<IEnumerable<Rule>, Func<Packet, U3>> partiallyAppliedFilterOne = rs => packet => FilterOne(rs, packet);
+
             var tell1 = new ListMonoid<Entry>(new[] {new Entry(1, "STARTING PACKET FILTER")});
             var tell2 = new ListMonoid<Entry>(new[] {new Entry(1, "STOPPING PACKET FILTER")});
-            return null;
+
+            return WriterEntry.Tell(tell1).BindIgnoringLeft(
+                GroupSame(new ListMonoid<Entry>(), MergeEntries, packets, partiallyAppliedFilterOne(rules)).Bind(
+                    @out => WriterEntry.Tell(tell2).BindIgnoringLeft(
+                        WriterEntry.Return(Maybe.CatMaybes(@out)))));
         }
+
+        // TODO:
+        // override ToString for Packet
+        // override Equals/GetHashCode for Packet
+        //
+        // override ToString for Rule
+        // 
+        // override ToString for AddrBase / AnyHost / Addr
+        // override Equals/GetHashCode for AddrBase / AnyHost / Addr
+        //
+        // override ToString for DataBase / AnyData / Data
+        // override Equals/GetHashCode for DataBase / AnyData / Data
 
         private static void Main()
         {
