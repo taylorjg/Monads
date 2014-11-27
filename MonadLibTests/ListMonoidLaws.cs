1e0ebe08f2d82cdefa91c2e5b9fe9087bee40670
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-11-27 16:40:31 +0000

    Added unit tests re Maybe's monad plus laws and ListMonoid's monoid laws

---
+++ MonadLibTests/ListMonoidLaws.cs
@@ -0,0 +1,49 @@
+﻿using MonadLib;
+using NUnit.Framework;
+
+namespace MonadLibTests
+{
+    // ReSharper disable InconsistentNaming
+
+    [TestFixture]
+    public class ListMonoidLaws
+    {
+        private ListMonoidAdapter<int> _listMonoidAdapter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _listMonoidAdapter = new ListMonoidAdapter<int>();
+        }
+
+        [Test]
+        public void MAppendOfMEmptyAndXIsX()
+        {
+            // mempty <> x = x
+            var x = new ListMonoid<int>(new[] { 1, 2, 3 });
+            var actual = (ListMonoid<int>)_listMonoidAdapter.MAppend(_listMonoidAdapter.MEmpty, x);
+            Assert.That(actual.List, Is.EqualTo(x.List));
+        }
+
+        [Test]
+        public void MAppendOfXAndMEmptyIsX()
+        {
+            // x <> mempty = x
+            var x = new ListMonoid<int>(new[] {1, 2, 3});
+            var actual = (ListMonoid<int>) _listMonoidAdapter.MAppend(x, _listMonoidAdapter.MEmpty);
+            Assert.That(actual.List, Is.EqualTo(x.List));
+        }
+
+        [Test]
+        public void MAppendAssociativity()
+        {
+            // x <> (y <> z) = (x <> y) <> z
+            var x = new ListMonoid<int>(new[] {1, 2, 3});
+            var y = new ListMonoid<int>(new[] {4, 5, 6});
+            var z = new ListMonoid<int>(new[] {7, 8, 9});
+            var actual1 = (ListMonoid<int>) _listMonoidAdapter.MAppend(x, _listMonoidAdapter.MAppend(y, z));
+            var actual2 = (ListMonoid<int>) _listMonoidAdapter.MAppend(_listMonoidAdapter.MAppend(x, y), z);
+            Assert.That(actual1.List, Is.EqualTo(actual2.List));
+        }
+    }
+}
---
+++ MonadLibTests/MaybeMonadPlusLaws.cs
@@ -0,0 +1,49 @@
+﻿using MonadLib;
+using NUnit.Framework;
+
+namespace MonadLibTests
+{
+    // ReSharper disable InconsistentNaming
+
+    [TestFixture]
+    internal class MaybeMonadPlusLaws
+    {
+        private MonadPlusAdapter<int> _monadPlusAdapter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _monadPlusAdapter = Maybe.Nothing<int>().GetMonadPlusAdapter();    
+        }
+
+        [Test]
+        public void MPlusOfMZeroAndMIsM()
+        {
+            // mzero `mplus` m  =  m
+            var m = Maybe.Just(42);
+            var actual = (Maybe<int>)_monadPlusAdapter.MPlus(_monadPlusAdapter.MZero, m);
+            Assert.That(actual, Is.EqualTo(m));
+        }
+
+        [Test]
+        public void MPlusOfMAndMZeroIsM()
+        {
+            // m `mplus` mzero  =  m
+            var m = Maybe.Just(42);
+            var actual = (Maybe<int>)_monadPlusAdapter.MPlus(m, _monadPlusAdapter.MZero);
+            Assert.That(actual, Is.EqualTo(m));
+        }
+
+        [Test]
+        public void MPlusAssociativity()
+        {
+            // m `mplus` (n `mplus` o)  =  (m `mplus` n) `mplus` o
+            var m = Maybe.Just(42);
+            var n = Maybe.Just(43);
+            var o = Maybe.Just(44);
+            var actual1 = (Maybe<int>) _monadPlusAdapter.MPlus(m, _monadPlusAdapter.MPlus(n, o));
+            var actual2 = (Maybe<int>) _monadPlusAdapter.MPlus(_monadPlusAdapter.MPlus(m, n), o);
+            Assert.That(actual1, Is.EqualTo(actual2));
+        }
+    }
+}
