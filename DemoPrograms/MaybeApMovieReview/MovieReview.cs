c00f75e8b474c5877869ecbdda9fae44ceb1d6d8
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-12-15 22:15:54 +0000

    Added another MoveReview demo program that uses LiftM and Ap

---
+++ DemoPrograms/MaybeApMovieReview/MovieReview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MaybeApMovieReview
+{
+    public class MovieReview
+    {
+        public string Title { get; set; }
+        public string User { get; set; }
+        public string Review { get; set; }
+
+        public string Format()
+        {
+            return string.Format(
+                "{{Title: {0}; User: {1}; Review: {2}}}",
+                Title,
+                User,
+                Review);
+        }
+
+        public static string Format(MovieReview movieReview)
+        {
+            return movieReview.Format();
+        }
+
+        public static MovieReview MakeMovieReview(string title, string user, string review)
+        {
+            return new MovieReview
+            {
+                Title = title,
+                User = user,
+                Review = review
+            };
+        }
+
+        public static Func<string, string, string, MovieReview> MakeMovieReviewFunc {
+            get { return MakeMovieReview; }
+        }
+    }
+}
---
+++ DemoPrograms/MaybeApMovieReview/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using MonadLib;
+
+namespace MaybeApMovieReview
+{
+    internal class Program
+    {
+        private static Maybe<string> Lookup1(string key, Dictionary<string, Maybe<string>> alist)
+        {
+            return alist
+                .GetValue(key)
+                .Bind(v => v.MFilter(s => !string.IsNullOrEmpty(s)));
+        }
+
+        private static Maybe<MovieReview> ApReview(Dictionary<string, Maybe<string>> alist)
+        {
+            return Lookup1("review", alist)
+                .Ap(Lookup1("user", alist)
+                .Ap(Maybe.LiftM(Fn.Curry(MovieReview.MakeMovieReviewFunc), Lookup1("title", alist))));
+        }
+
+        private static void Main()
+        {
+            // All keys present and correct
+            Print(ApReview(new Dictionary<string, Maybe<string>>
+                {
+                    {"title", Maybe.Just("Jaws")},
+                    {"user", Maybe.Just("Jon")},
+                    {"review", Maybe.Just("A film about a shark")}
+                }));
+
+            // Missing "user" key
+            Print(ApReview(new Dictionary<string, Maybe<string>>
+                {
+                    {"title", Maybe.Just("Jaws")},
+                    {"review", Maybe.Just("A film about a shark")}
+                }));
+
+            // Value of "user" key is empty
+            Print(ApReview(new Dictionary<string, Maybe<string>>
+                {
+                    {"title", Maybe.Just("Jaws")},
+                    {"user", Maybe.Just(string.Empty)},
+                    {"review", Maybe.Just("A film about a shark")}
+                }));
+
+            // Value of "user" key is Nothing
+            Print(ApReview(new Dictionary<string, Maybe<string>>
+                {
+                    {"title", Maybe.Just("Jaws")},
+                    {"user", Maybe.Nothing<string>()},
+                    {"review", Maybe.Just("A film about a shark")}
+                }));
+        }
+
+        private static void Print(Maybe<MovieReview> movieReview)
+        {
+            Console.WriteLine(movieReview.Match(MovieReview.Format, () => "Nothing"));
+        }
+    }
+}
